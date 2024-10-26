448e2c6b32c8cccea59a89d8e1d3b5adff29ca18
Shai <157624190+shaicoin@users.noreply.github.com>
2024-10-26 03:00:06 -0400

    Lengthen share target

--- services/difficulty_adjustment_service.js
+++ services/difficulty_adjustment_service.js
@@ -2,7 +2,7 @@ const submissionTimestamps = {};
 const kalmanFilters = {};
 const rollingSubmissionTimes = {};
 
-const targetRate = 333;
+const targetRate = 420;
 const maxRollingWindow = 10;
 
 const initializeKalmanFilter = () => {
